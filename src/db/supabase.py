01f527b451b9b378f38d319195abde521946f805
Wandres1 <75052563+Wandres1@users.noreply.github.com>
2023-06-09 12:34:33 -0500

    Delete src directory

--- src/db/supabase.py
+++
@@ -1,6 +0,0 @@
-import supabase
-
-SUPABASE_URL = 'https://xeudcfxkwzjnmbjlyorr.supabase.co'
-SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhldWRjZnhrd3pqbm1iamx5b3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0MDYxNDcsImV4cCI6MTk5OTk4MjE0N30.9G3UQp_QKsfXTTMlI8jVYudqeTFyHyrzhlkhsNHT6Ok'
-
-supabase = supabase.Client(SUPABASE_URL, SUPABASE_KEY)
--- src/models/ModelUsuario.py
+++
@@ -1,7 +0,0 @@
-class ModelUsuario():
-
-    def login(self, db, user):
-        try:
-            cursor=db.connection.cursor()
-        except:
-            pass
